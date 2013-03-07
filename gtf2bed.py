a166e05c47c3d5a064842314018dffb811a09dcf
David Kelley <dkelley@fas.harvard.edu>
2013-03-07 14:27:31 -0500

    Bug fix

--- gtf2bed.py
+++ gtf2bed.py
@@ -31,13 +31,13 @@ def main():
         if options.cds:
             block_sizes = ','.join([str(ex.end-ex.start+1) for ex in g.cds])
             block_starts = ','.join([str(ex.start-g.cds[0].start) for ex in g.cds])
-        
+
             cols = [g.chrom, str(g.cds[0].start-1), str(g.cds[-1].end), transcript_id, '0', g.strand, '0', '0', '255,0,0', str(len(g.cds)), block_sizes, block_starts]
 
         else:
             block_sizes = ','.join([str(ex.end-ex.start+1) for ex in g.exons])
             block_starts = ','.join([str(ex.start-g.exons[0].start) for ex in g.exons])
-        
+
             cols = [g.chrom, str(g.exons[0].start-1), str(g.exons[-1].end), transcript_id, '0', g.strand, '0', '0', '255,0,0', str(len(g.exons)), block_sizes, block_starts]
 
         print '\t'.join(cols)
