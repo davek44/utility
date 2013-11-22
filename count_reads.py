fe94d09c80360a0817f86da63ffa3ef5975af68f
David Kelley <dkelley@fas.harvard.edu>
2013-11-21 20:06:49 -0500

    Added a GTF restricted count

--- count_reads.py
+++ count_reads.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from optparse import OptionParser
+import os, subprocess, tempfile
 import pysam
 
 ################################################################################
@@ -23,19 +24,50 @@ def main():
 
     print count(args[0])
 
+
+################################################################################
+# count
+################################################################################
 def count(bam_file, mapq=False):
     # initialize
-    count = 0.0
+    bam_count = 0.0
 
     # process
-    for aligned_read in pysam.Samfile(bam_file):
+    for aligned_read in pysam.Samfile(bam_file, 'rb'):
         if mapq == False or aligned_read.mapq > 0:
-            if aligned_read.is_paired:
-                count += 0.5/aligned_read.opt('NH')
-            else:
-                count += 1.0/aligned_read.opt('NH')
+            try:
+                nh_tag = aligned_read.opt('NH')
+            except:
+                nh_tag = 1
 
-    return count
+            if aligned_read.is_paired:
+                bam_count += 0.5/nh_tag
+            else:
+                bam_count += 1.0/nh_tag
+
+    return bam_count
+
+
+################################################################################
+# count_gtf
+#
+# WARNING:
+#  I'm half assing by not considering strand information.
+################################################################################
+def count_gtf(bam_file, ref_gtf, mapq=False):
+    ref_bam_fd, ref_bam_file = tempfile.mkstemp(dir='%s/research/scratch/temp' % os.environ['HOME'])
+
+    # intersect
+    subprocess.call('intersectBed -abam %s -b %s > %s' % (bam_file, ref_gtf, ref_bam_file), shell=True)
+
+    # count
+    bam_count = count(ref_bam_file, mapq)
+
+    # clean
+    os.close(ref_bam_fd)
+    os.remove(ref_bam_file)
+
+    return bam_count
 
 
 ################################################################################
