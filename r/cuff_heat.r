ab0aa8cc7b50cf5c3b0092e8cf6c37410535ef14
David Kelley <dkelley@fas.harvard.edu>
2014-01-17 20:27:12 -0500

    spacing

--- r/cuff_heat.r
+++ r/cuff_heat.r
@@ -20,11 +20,11 @@ sample.order = colnames(fpkm.matrix)[sample.clust$order]
 gene_breaks = (1:nrow(fpkm.matrix))-0.5
 
 ggplot(df, aes(x=Sample, y=Gene, fill=FPKM)) +
- geom_tile() +
- scale_x_discrete("", limits=sample.order) + 
- scale_y_discrete(breaks=gene_breaks, limits=gene.order, labels=rep("",nrow(fpkm.matrix))) +
- scale_fill_gradient("FPKM", low="white", high="darkred") +
- theme(text = element_text(size=30), axis.text.x=element_text(size=30)) +
- theme_bw()
+    geom_tile() +
+    scale_x_discrete("", limits=sample.order) + 
+    scale_y_discrete(breaks=gene_breaks, limits=gene.order, labels=rep("",nrow(fpkm.matrix))) +
+    scale_fill_gradient("FPKM", low="white", high="tomato3") +
+    theme_bw() +
+    theme(axis.text.x=element_text(angle=315, hjust=0, vjust=1))
 
 ggsave(output.pdf)
