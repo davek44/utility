67623c056d89588d0ce109aff731168a425c1c1a
David Kelley <dkelley@fas.harvard.edu>
2014-01-17 20:24:04 -0500

    Option for just signif genes

--- cuff_heat.py
+++ cuff_heat.py
@@ -8,18 +8,17 @@ import cufflinks, gff, ggplot
 #
 ################################################################################
 
-log_min = .25
-
 ################################################################################
 # main
 ################################################################################
 def main():
     usage = 'usage: %prog [options] <fpkm_tracking>'
     parser = OptionParser(usage)
+    parser.add_option('-d', dest='diff_file', help='Limit to significantly differentially expressed genes')
     parser.add_option('-g', dest='gtf', help='GTF file of genes to display')
     parser.add_option('-m', dest='min_fpkm', default=.125, help='Minimum FPKM (for logs) [Default: %default]')
     parser.add_option('-o', dest='out_pdf', default='cuff_heat.pdf', help='Output PDF [Default: %default]')
-    parser.add_option('-s', dest='sample', default=50, help='Sample genes rather than use all [Default: %default]')
+    parser.add_option('-s', dest='sample', default=1000, help='Sample genes rather than use all [Default: %default]')
     (options,args) = parser.parse_args()
 
     if len(args) != 1:
@@ -31,12 +30,17 @@ def main():
     cuff = cufflinks.fpkm_tracking(fpkm_file=fpkm_tracking)
 
     # determine genes
-    all_genes = cuff.genes
+    all_genes = set(cuff.genes)
     if options.gtf:
         all_genes = set()
         for line in open(options.gtf):
             a = line.split('\t')
             all_genes.add(gff.gtf_kv(a[8])['gene_id'])
+
+    if options.diff_file:
+        # limit to differentially expressed genes
+        diff_genes = find_diff(options.diff_file)
+        all_genes &= diff_genes
 
     # sample genes to display
     if len(all_genes) <= options.sample:
@@ -56,7 +60,33 @@ def main():
                 df['FPKM'].append(math.log(ge[i]+options.min_fpkm,2))
 
     # plot
-    ggplot.plot('%s/cuff_heat.r' % os.environ['RDIR'], df, [options.out_pdf], debug=True)
+    ggplot.plot('%s/cuff_heat.r' % os.environ['RDIR'], df, [options.out_pdf])
+
+
+################################################################################
+# find_diff
+#
+# Return a set of only the differentially expressed genes.
+################################################################################
+def find_diff(diff_file):
+    diff_genes = set()
+
+    diff_in = open(diff_file)
+    diff_in.readline()
+
+    for line in diff_in:
+        a = line.split('\t')
+        a[-1] = a[-1].rstrip()
+
+        gene_id = a[0]
+        sig = a[-1]
+
+        if sig == 'yes':
+            diff_genes.add(gene_id)
+
+    diff_in.close()
+
+    return diff_genes
 
 
 ################################################################################
