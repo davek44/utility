31bb32a86c4edecfae84bb8070f5f5e874ed1230
David Kelley <dkelley@fas.harvard.edu>
2014-03-16 21:37:08 -0400

    df print

--- ggplot.py
+++ ggplot.py
@@ -12,12 +12,12 @@ import os, subprocess, sys, tempfile
 ################################################################################
 # plot
 ################################################################################
-def plot(r_script, df_dict, args, debug=False):
+def plot(r_script, df_dict, args, df_file=None):
     # open temp file
-    if debug:
-        df_file = 'data_frame.txt'
-    else:
+    if df_file == None:
         df_fd, df_file = tempfile.mkstemp()
+    else:
+        df_fd = None
     df_out = open(df_file, 'w')
 
     # get headers
@@ -43,6 +43,34 @@ def plot(r_script, df_dict, args, debug=False):
     subprocess.call('R --slave --args %s %s < %s' % (df_file, args_str, r_script), shell=True)
 
     # clean
-    if not debug:
+    if df_fd != None:
         os.close(df_fd)
         os.remove(df_file)
+
+
+################################################################################
+# print_df
+#
+# Just print the given data frame dictionary to the output file given.
+################################################################################
+def print_df(df_dict, out_file):
+    # open
+    df_out = open(out_file, 'w')
+
+    # get headers
+    headers = sorted(df_dict.keys())
+    print >> df_out, ' '.join([str(head) for head in headers])
+
+    # check list lengths
+    length = len(df_dict[headers[0]])
+    for i in range(1,len(headers)):
+        if length != len(df_dict[headers[i]]):
+            print >> sys.stderr, 'Lists in dict vary in length:'
+            for j in range(len(headers)):
+                print >> sys.stderr, headers[j], len(df_dict[headers[j]])
+            exit(1)
+
+    # print data frame
+    for i in range(length):
+        print >> df_out, ' '.join([str(df_dict[head][i]) for head in headers])
+    df_out.close()
