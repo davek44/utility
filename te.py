d2626d6af42f8c735651d133eabcc4787c0a5413
David Kelley <dkelley@fas.harvard.edu>
2014-04-29 17:35:15 -0400

    Intersect genes, repeats method

--- te.py
+++ te.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from optparse import OptionParser
-import glob, os, sys
+import glob, os, subprocess, sys
 import gff
 
 ################################################################################
@@ -18,7 +18,106 @@ def main():
     parser = OptionParser(usage)
     #parser.add_option()
     (options,args) = parser.parse_args()
+
+
+################################################################################
+# hash_genes_repeats
+#
+# Hash genes in gtf_file to sets of repeats in repeats_gff.
+################################################################################
+def hash_genes_repeats(gtf_file, repeats_gff, gene_key='gene_id', add_star=True, stranded=False):
+    gene_repeats = {}
     
+    p = subprocess.Popen('intersectBed -wo -a %s -b %s' % (gtf_file, repeats_gff), shell=True, stdout=subprocess.PIPE)
+    line = p.stdout.readline()
+    while line:
+        a = line.split('\t')
+
+        # get names
+        gene_id = gff.gtf_kv(a[8])[gene_key]
+        rep_kv = gff.gtf_kv(a[17])
+        rep = rep_kv['repeat']
+        fam = rep_kv['family']
+
+        # get strands
+        gene_strand = a[6]
+        te_strand = a[15]
+
+        if stranded:
+            if gene_strand == te_strand:
+                orient = '+'
+            else:
+                orient = '-'
+
+            gene_repeats.setdefault(gene_id,set()).add((rep,fam,orient))
+            if add_star:
+                gene_repeats[gene_id].add(('*',fam,orient))
+                gene_repeats[gene_id].add(('*','*',orient))
+
+        else:
+            gene_repeats.setdefault(gene_id,set()).add((rep,fam))
+            if add_star:
+                gene_repeats[gene_id].add(('*',fam))
+                gene_repeats[gene_id].add(('*','*'))
+
+        line = p.stdout.readline()
+    p.communicate()
+
+    return gene_repeats
+
+
+################################################################################
+# hash_genes_repeats_nt
+#
+# Hash genes in gtf_file to a dict mapping repeats in repeats_gff to nt overlaps.
+#
+# Warnings:
+#  -If we hash by gene_id, we want to have chosen a single isoform per gene.
+################################################################################
+def hash_genes_repeats_nt(gtf_file, repeats_gff, gene_key='gene_id', add_star=True):
+    gene_repeat_nt = {}
+
+    p = subprocess.Popen('intersectBed -wo -a %s -b %s' % (gtf_file, repeats_gff), shell=True, stdout=subprocess.PIPE)
+    line = p.stdout.readline()
+    while line:
+        a = line.split('\t')
+
+        # get names
+        gene_id = gff.gtf_kv(a[8])['gene_id']
+        rep_kv = gff.gtf_kv(a[17])
+        rep = rep_kv['repeat']
+        fam = rep_kv['family']
+
+        # get overlap
+        nt_overlap = int(a[18])
+
+        if not gene_id in gene_repeat_nt:
+            gene_repeat_nt[gene_id] = {}
+
+        gene_repeat_nt[gene_id][(rep,fam)] = gene_repeat_nt[gene_id].get((rep,fam),0) + nt_overlap
+        if add_star:
+            gene_repeat_nt[gene_id][('*',fam)] = gene_repeat_nt[gene_id].get(('*',fam),0) + nt_overlap
+            gene_repeat_nt[gene_id][('*','*')] = gene_repeat_nt[gene_id].get(('*','*'),0) + nt_overlap
+
+        line = p.stdout.readline()
+    p.communicate()
+
+    return gene_repeat_nt
+
+
+################################################################################
+# hash_repeat_family
+#
+# Hash repeat -> family from the RepeatMasker GFF.
+################################################################################
+def hash_repeat_family():
+    repeat_family = {}
+    for line in open('%s/hg19.fa.out.tp.gff' % os.environ['MASK']):
+        a = line.split('\t')
+        kv = gff.gtf_kv(a[8])
+        repeat_family[kv['repeat']] = kv['family']
+    return repeat_family
+
 
 ################################################################################
 # map_dfam_family
@@ -96,20 +195,6 @@ def map_rm_dfam(repeat, quiet=False):
                 dfam_reps.append(hmm_files[i][start:end])
 
     return dfam_reps
-
-
-################################################################################
-# hash_repeat_family
-#
-# Hash repeat -> family from the RepeatMasker GFF.
-################################################################################
-def hash_repeat_family():
-    repeat_family = {}
-    for line in open('%s/hg19.fa.out.tp.gff' % os.environ['MASK']):
-        a = line.split('\t')
-        kv = gff.gtf_kv(a[8])
-        repeat_family[kv['repeat']] = kv['family']
-    return repeat_family
 
 
 ################################################################################
