4d0aebe58b00cef61be9b80774dd10286c4edf53
David Kelley <dkelley@fas.harvard.edu>
2014-08-21 12:39:42 -0400

    font

--- r/peak_bam_plot.r
+++ r/peak_bam_plot.r
@@ -7,9 +7,10 @@ output.pdf = ca[2]
 df = read.table(df.file, header=T, quote="\"")
 
 ggplot(df, aes(x=peak_i, y=cov)) +
- geom_point() +
- scale_x_continuous("Peak index") +
- scale_y_continuous("Coverage") +
- theme_bw()
+    geom_point() +
+    scale_x_continuous("Peak index") +
+    scale_y_continuous("Coverage") +
+    theme_bw() +
+    theme(text=element_text(size=20))
 
 ggsave(output.pdf)
