308b36617c84d2a4b5123a2d70da1e2cdf903b3c
David Kelley <dkelley@fas.harvard.edu>
2014-08-21 12:28:10 -0400

    fudge up normalized cov and add labels

--- r/plot_gff_cov_meta.r
+++ r/plot_gff_cov_meta.r
@@ -4,6 +4,8 @@ library(plyr)
 ca = commandArgs(trailing=T)
 df.file = ca[1]
 out.pre = ca[2]
+label.primary = ca[3]
+label.control = ca[4]
 
 df = read.table(df.file, header=T, quote="\"")
 
@@ -12,51 +14,49 @@ if (ncol(df) == 2) {
     gp = ggplot(df, aes(x=Index, y=Coverage))
 } else {
     gp = ggplot(df, aes(x=Index, y=Coverage, color=Type)) +
-        scale_color_manual("", values=c("#F46D43", "#66BD63"))
+        scale_color_manual("", values=c("#F46D43", "#66BD63"), breaks=c("Primary","Control"), labels=c(label.primary, label.control))
 
         #scale_color_brewer(palette="Set1")
 }
 
 gp +
     geom_point() +
-    geom_smooth() +
     theme_bw() +
-    theme(text=element_text(size=20)) +
+    theme(text=element_text(size=18)) +
     theme(legend.justification=c(1,0), legend.position=c(1,0))
+
+#     geom_smooth(span=0.1) +
 
 ggsave(paste(out.pre,"_raw.pdf",sep=""))
 
 
 # normalized
-if (ncol(df) == 2) {
-    cov.sum = sum(df$Coverage)
-    df$Coverage.Norm = df$Coverage / cov.sum
+if (ncol(df) > 2) {
+    # the values are so low, I want to boost them up
+    fudge=10
 
-    gp = ggplot(df, aes(x=Index, y=Coverage.Norm))
-} else {
     control.sum = sum(df[df$Type=="Control",]$Coverage)
     primary.sum = sum(df[df$Type!="Control",]$Coverage)
 
     df$Coverage.Norm = df$Coverage
     for (i in 1:nrow(df)) {
         if (df[i,"Type"] == "Control") {
-            df[i,"Coverage.Norm"] = df[i,"Coverage"] / control.sum
+            df[i,"Coverage.Norm"] = fudge * df[i,"Coverage"] / control.sum
         } else {
-            df[i,"Coverage.Norm"] = df[i,"Coverage"] / primary.sum
+            df[i,"Coverage.Norm"] = fudge * df[i,"Coverage"] / primary.sum
         }
     }
 
-    gp = ggplot(df, aes(x=Index, y=Coverage.Norm, color=Type)) +
-        scale_color_manual("", values=c("#F46D43", "#66BD63"))
-        #scale_color_brewer(palette="Set1")
+    ggplot(df, aes(x=Index, y=Coverage.Norm, color=Type)) +
+        scale_color_manual("", values=c("#F46D43", "#66BD63"), breaks=c("Primary","Control"), labels=c(label.primary, label.control))
+        geom_point() +
+        scale_y_continuous("Normalized coverage") +
+        theme_bw() +
+        theme(text=element_text(size=18)) +
+        theme(legend.justification=c(1,0), legend.position=c(1,0))
+
+# scale_color_brewer(palette="Set1")
+# geom_smooth(span=0.1) +
+
+    ggsave(paste(out.pre,"_norm.pdf",sep=""))
 }
-
-gp +
-    geom_point() +
-    geom_smooth() +
-    scale_y_continuous("Normalized coverage") +
-    theme_bw() +
-    theme(text=element_text(size=20)) +
-    theme(legend.justification=c(1,0), legend.position=c(1,0))
-
-ggsave(paste(out.pre,"_norm.pdf",sep=""))
