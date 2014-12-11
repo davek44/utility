bd7b30a4a0e94b99741dba0312b7e8d3f7df8fde
David Kelley <dkelley@fas.harvard.edu>
2014-12-11 17:17:05 -0500

    nm

--- r/annotation_pie_pie.r
+++ r/annotation_pie_pie.r
@@ -2,12 +2,11 @@ library(ggplot2)
 
 ca = commandArgs(trailing=T)
 df.file = ca[1]
-plot.title = ca[2]
-output.pdf = ca[3]
+output.pdf = ca[2]
 
 df = read.table(df.file, header=T, quote="\"")
 
-annotation.order.all = c('Intergenic','Introns','3\'UTR','5\'UTR','CDS','lncRNA','Pseudogene','rRNA','smallRNA')
+annotation.order.all = c('Intergenic','Introns','Exons','3\'UTR','5\'UTR','CDS','lncRNA','Pseudogene','rRNA','smallRNA')
 annotation.order = annotation.order.all[annotation.order.all %in% df$annotation]
 df$annotation = factor(df$annotation, levels=annotation.order)
 
@@ -17,7 +16,6 @@ ggplot(df, aes(x=dummy, y=count, fill=annotation)) +
  scale_x_discrete("") +
  scale_y_continuous("") +
  scale_fill_discrete("Annotation") +
- ggtitle(plot.title) +
  theme_bw() +  
  theme(axis.text.x=element_blank(), axis.text.y=element_blank(), axis.ticks=element_blank(), panel.grid.minor=element_blank(), panel.grid.major=element_blank())
 
