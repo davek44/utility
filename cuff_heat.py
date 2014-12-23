382aeb3b09bc6ea5bff55aa05922c23cea7263f4
David Kelley <dkelley@fas.harvard.edu>
2014-12-23 11:39:42 -0500

    Min fpkm and aesthetic changes

--- cuff_heat.py
+++ cuff_heat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from optparse import OptionParser
+import numpy as np
 import math, os, pdb, random, sys
 import cufflinks, gff, ggplot
 
@@ -14,11 +15,14 @@ import cufflinks, gff, ggplot
 def main():
     usage = 'usage: %prog [options] <fpkm_tracking>'
     parser = OptionParser(usage)
+    parser.add_option('-a', dest='max_fpkm', type='float', help='Maxium log2 FPKM to plot [Default: %d]')
     parser.add_option('-d', dest='diff_file', help='Limit to significantly differentially expressed genes')
     parser.add_option('-g', dest='gtf', help='GTF file of genes to display')
-    parser.add_option('-m', dest='min_fpkm', default=.125, help='Minimum FPKM (for logs) [Default: %default]')
+    parser.add_option('-m', dest='min_fpkm', default=0, type='float', help='Minimum FPKM [Default: %default]')
+    parser.add_option('-p', dest='pseudocount', default=.125, type='float', help='Pseudocount for log FPKM [Default: %default]')
     parser.add_option('-o', dest='out_pdf', default='cuff_heat.pdf', help='Output PDF [Default: %default]')
-    parser.add_option('-s', dest='sample', default=1000, help='Sample genes rather than use all [Default: %default]')
+    parser.add_option('-s', dest='sample', default=1000, type='int', help='Sample genes rather than use all [Default: %default]')
+    parser.add_option('-u', dest='uppercase', default=False, action='store_true', help='Uppercase sample labels [Default: %default]')
     (options,args) = parser.parse_args()
 
     if len(args) != 1:
@@ -37,10 +41,36 @@ def main():
             a = line.split('\t')
             all_genes.add(gff.gtf_kv(a[8])['gene_id'])
 
+
     if options.diff_file:
         # limit to differentially expressed genes
         diff_genes = find_diff(options.diff_file)
         all_genes &= diff_genes
+
+    else:
+        # at least limit to clean genes
+        clean_genes = set()
+        for gene_id in all_genes:
+            ge = cuff.gene_expr(gene_id)
+            clean = True 
+            for i in range(len(ge)):
+                if math.isnan(ge[i]):
+                    clean = False
+                    break
+            if clean:
+                clean_genes.add(gene_id)
+
+        all_genes &= clean_genes
+
+
+    if options.min_fpkm > 0:
+        expressed_genes = set()
+        for gene_id in all_genes:
+            ge = cuff.gene_expr(gene_id, not_found=0, fail=0)
+            if max(ge) > options.min_fpkm:
+                expressed_genes.add(gene_id)
+                
+        all_genes &= expressed_genes
 
     # sample genes to display
     if len(all_genes) <= options.sample:
@@ -52,15 +82,23 @@ def main():
     df = {'Gene':[], 'FPKM':[], 'Sample':[]}
 
     for gene_id in display_genes:
-        ge = cuff.gene_expr(gene_id)
-        if not math.isnan(ge[0]):
-            for i in range(len(cuff.experiments)):
-                df['Gene'].append(gene_id)
-                df['Sample'].append(cuff.experiments[i])
-                df['FPKM'].append(math.log(ge[i]+options.min_fpkm,2))
+        ge = cuff.gene_expr(gene_id, not_found=0, fail=0)
+
+        for i in range(len(cuff.experiments)):
+            df['Gene'].append(gene_id)
+
+            df['Sample'].append(cuff.experiments[i])
+            if options.uppercase:
+                df['Sample'][-1] = df['Sample'][-1].upper()
+
+            logfpkm = np.log2(ge[i]+options.pseudocount)
+            if options.max_fpkm:
+                logfpkm = min(options.max_fpkm, logfpkm)
+            df['FPKM'].append(logfpkm)
 
     # plot
-    ggplot.plot('%s/cuff_heat.r' % os.environ['RDIR'], df, [options.out_pdf])
+    out_df = '%s.df' % options.out_pdf[:-4]
+    ggplot.plot('%s/cuff_heat.r' % os.environ['RDIR'], df, [options.out_pdf], df_file=out_df)
 
 
 ################################################################################
--- r/cuff_heat.r
+++ r/cuff_heat.r
@@ -1,5 +1,7 @@
 library(ggplot2)
 library(reshape2)
+library(seriation)
+library(RColorBrewer)
 
 ca = commandArgs(trailing=T)
 df.file = ca[1]
@@ -10,19 +12,26 @@ df = read.table(df.file, header=T, quote="\"")
 fpkm.matrix = acast(df, Gene ~ Sample, value.var="FPKM")
 
 gene.dist = dist(fpkm.matrix)
-gene.clust = hclust(gene.dist)
-gene.order = rownames(fpkm.matrix)[gene.clust$order]
+#gene.clust = hclust(gene.dist)
+#gene.order = rownames(fpkm.matrix)[gene.clust$order]
+gene.ser = seriate(gene.dist, method="OLO")
+gene.order = rownames(fpkm.matrix)[get_order(gene.ser)]
 
 sample.dist = dist(t(fpkm.matrix))
-sample.clust = hclust(sample.dist)
-sample.order = colnames(fpkm.matrix)[sample.clust$order]
+#sample.clust = hclust(sample.dist)
+#sample.order = colnames(fpkm.matrix)[sample.clust$order]
+sample.ser = seriate(sample.dist, method="OLO")
+sample.order = colnames(fpkm.matrix)[get_order(sample.ser)]
 
 ggplot(df, aes(x=Sample, y=Gene, fill=FPKM)) +
     geom_tile() +
     scale_x_discrete("", limits=sample.order) + 
     scale_y_discrete(limits=gene.order) +
-    scale_fill_gradient("FPKM", low="white", high="tomato3") +
+    scale_fill_gradientn("FPKM", colours=c("white", brewer.pal(8, "YlOrRd"))) +
     theme_bw() +
+    theme(text=element_text(size=20)) +
     theme(axis.text.x=element_text(angle=315, hjust=0, vjust=1), axis.ticks.y=element_blank(), axis.text.y=element_blank())
 
 ggsave(output.pdf)
+
+# scale_fill_gradientn("FPKM", colours=c("white", brewer.pal(8, "YOrRd"))) +
