1c461850eaa3bfd01214e578533d80accc927b6d
David Kelley <dkelley@fas.harvard.edu>
2015-07-04 15:47:03 -0400

    color change

--- r/cuff_bar.r
+++ r/cuff_bar.r
@@ -15,14 +15,18 @@ if (y.max == "None") {
 	y.max = as.numeric(y.max)
 }
 
+color.count = length(unique(df$Sample))
+get.pal = colorRampPalette(brewer.pal(11, "Spectral"))
+
 ggplot(df, aes(x=Sample, y=FPKM, ymin=conf_lo, ymax=conf_hi, fill=Sample)) +
-	geom_bar(stat="identity") +
+	geom_bar(stat="identity", fill=get.pal(color.count)) +
 	geom_errorbar(width=0.5) +
 	scale_x_discrete("", limits=df$Sample) +
-	scale_fill_brewer(palette="Spectral") +
 	guides(fill=FALSE) +
 	theme_bw() +
 	theme(text=element_text(size=22)) +
 	theme(axis.text.x=element_text(angle=315, hjust=0, vjust=1))
+
+# 	scale_fill_brewer(palette="Spectral") +
 
 ggsave(output.pdf)
