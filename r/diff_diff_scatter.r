8500c6dad48927ca44e5b4a90273d4e1a45cf42e
David Kelley <dkelley@fas.harvard.edu>
2015-07-05 18:41:42 -0400

    larger font

--- r/diff_diff_scatter.r
+++ r/diff_diff_scatter.r
@@ -14,11 +14,13 @@ y.max = quantile(df$diff2, .998)
 
 ggplot(df, aes(x=diff1, y=diff2)) +
     geom_point(size=1.5, alpha=.3) +
-    stat_smooth() +
+    stat_smooth(method="lm") +
     scale_x_continuous("Test stat 1", lim=c(x.min,x.max)) +
     scale_y_continuous("Test stat 2", lim=c(y.min,y.max)) +
     geom_abline(intercept=0, slope=1, linetype=2) +
     theme_bw() +
-    theme(text=element_text(size=18))
+    theme(text=element_text(size=25))
+
+# stat_smooth(method="lm") +
 
 ggsave(output.pdf)
