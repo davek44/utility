9c89cff3e2853015898ba744e69d44247168d223
David Kelley <drk@calicolabs.com>
2019-12-11 22:19:46 -0800

    print tree

--- h5_sum.py
+++ h5_sum.py
@@ -18,14 +18,21 @@ def main():
     (options,args) = parser.parse_args()
 
     h5_in = h5py.File(args[0], 'r')
+    print_h5_tree(h5_in)
 
-    h5_keys = sorted(list(h5_in.keys()))
-    for hkey in h5_keys:
-        print(h5_in[hkey])
+    # h5_keys = sorted(list(h5_in.keys()))
+    # for hkey in h5_keys:
+    #    print(h5_in[hkey])
 
     h5_in.close()
 
-
+def print_h5_tree(h5_obj, depth=0):
+    h5_keys = sorted(list(h5_obj.keys()))
+    for hkey in h5_keys:
+        print(''.join(['\t']*depth), h5_obj[hkey])
+        if type(h5_obj[hkey]) == h5py._hl.group.Group:
+            print_h5_tree(h5_obj[hkey], depth+1)
+    
 ################################################################################
 # __main__
 ################################################################################
